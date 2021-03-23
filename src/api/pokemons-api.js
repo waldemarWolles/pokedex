d440e29cd0931c2926c845d3149c0488f4e8eb1e
Waldemar <chooter1999@gmail.com>
2021-03-23 03:13:44 +0200

    fixed api

--- src/api/pokemons-api.js
+++ src/api/pokemons-api.js
@@ -2,22 +2,23 @@ import axios from 'axios'
 
 export const pokemonsAPI = {
   getPokemons: async (limit, offset) => {
-    debugger
     const response = await axios.get(
       `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`
     )
-    const requestsArray = response.data.results.map((request) => {
-      return axios.get(`${request.url}`)
+    const requestsArray = response.data.results.map(async (request, index) => {
+      const responseUrl = await axios.get(`${request.url}`)
+      if (responseUrl.status === 200) {
+        return responseUrl
+      } else {
+        return axios.get(`https://pokeapi.co/api/v2/pokemon/${index + 10}/`)
+      }
     })
-    debugger
     const responseAll = await axios.all(requestsArray)
     return responseAll
   },
 
   getTypes: async () => {
-    debugger
     const response = await axios.get(`https://pokeapi.co/api/v2/type?limit=999`)
-    debugger
     return response.data
   },
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
-import './../App.css'
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
